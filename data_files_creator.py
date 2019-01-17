f5d7823fae9e2fa2e0b8dafe788085f167c201d8
sdh2110 <sdh2110@g.rit.edu>
2019-01-17 10:16:16 -0500

    Added handling for run configuration data.

--- data_files_creator.py
+++ data_files_creator.py
@@ -1,10 +1,12 @@
 import os
 from web_browser import *
 from helper_functions import get_file_location
+import global_data
+from stats_packs import STANDARD_COUNT
 
 
 def create_opponents_profile(file_location, opponents_names):
-    stats_count = len(StandardPack.__slots__)
+    stats_count = STANDARD_COUNT
     blank_stats = "|0|0" + " 0" * (stats_count - 1)
     if os.path.isfile(file_location):
         print("A profile already exists at", file_location)
@@ -26,3 +28,20 @@ def create_all_profiles(year):
         opponents = teams[:]
         del opponents[i]
         create_opponents_profile(get_file_location(folder_name, teams[i]), opponents)
+
+
+def create_runconfig():
+    if os.path.isfile("runconfig.txt"):
+        print("runconfig.txt already exists")
+    else:
+        file = open("runconfig.txt", "w")
+        file.write("other season=0\n")
+        file.write("week num=1\n")
+        file.close()
+
+
+def update_runconfig():
+    file = open("runconfig.txt", "w")
+    for config in global_data.RUNCONFIGS:
+        file.write(config + "=" + str(global_data.RUNCONFIGS[config]) + "\n")
+    file.close()
