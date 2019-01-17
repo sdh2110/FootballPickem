6284c5638acc6ab0593878498de57535d485a785
sdh2110 <sdh2110@g.rit.edu>
2019-01-17 10:12:32 -0500

    Standalone script that updates local data based on games played.
    This script can be run by itself. It automatically reads in the correct
    week's schedule and updates opponent data according to the games that
    were played that week.

---
+++ fbpickem_post.py
@@ -0,0 +1,34 @@
+from game_stats import *
+import global_data
+import web_browser
+import opponent_strengths
+from helper_functions import get_file_location
+from data_files_creator import update_runconfig
+
+
+def main():
+    global_data.load_globals()
+
+    teams_played = {}
+    for team in global_data.CURRENT_TEAMS:
+        teams_played[team] = False
+
+    schedule = web_browser.load_schedule(global_data.CURRENT_YEAR, global_data.CURRENT_WEEK)
+    for game in schedule:
+        game_stats = web_browser.load_game_stats(game.game_url)
+        game_stats.render_game()
+        teams_played[game.vis_team] = True
+        teams_played[game.home_team] = True
+
+    for team in teams_played:
+        if teams_played[team] is False:
+            team_ops = OpponentStrengths(get_file_location(str(global_data.CURRENT_YEAR), team))
+            team_ops.phaseout_data()
+            team_ops.save_to_file()
+
+    global_data.RUNCONFIGS["week num"] += 1
+    update_runconfig()
+
+
+if __name__ == '__main__':
+    main()
